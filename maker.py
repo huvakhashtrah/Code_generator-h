600d533bce456bdf24d9de96286eda5a823a0b81
huvakhashtrah <160272826+huvakhashtrah@users.noreply.github.com>
2024-03-26 16:32:57 +0330

    Create maker.py

---
+++ maker.py
@@ -0,0 +1,46 @@
+import random
+from datetime import datetime
+
+print('\033[91m⠀⠀⠀⠀⠀⠀⢀⡠⠖⠚⠋⠉⠉⠉⠙⠒⢦⡀⠀⠀⠀⠀')
+print('⠀⠀⠀⠀⢠⠞⠉⠀⠀⠀⢠⣾⣿⡦⡀⠀⣴⣾⣧⣀⠀⠀')
+print('⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⣉⣁⣀⣀⣤⣿⣿⣿⣿⣦⠀')
+print('⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠙⠋⠉⠙⢻⣿⣿⣿⣿⣿⠀')
+print('⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠉⠉⠙⡟⠀')
+print('⠀⢸⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀')
+print('⠀⢈⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡀⡀⠳⡄⠀⠀⠀')
+print('⠀⣸⡴⠀⠀⠀⠀⠀⠀⡀⢠⡄⣄⣷⣿⣷⣷⣠⡘⣆⠀⠀')
+print('⢠⣿⣧⡞⣠⡄⣰⣇⣼⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀')
+print('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣧')
+print('⠋⠁⣿⡿⠟⣿⣿⣿⡿⣿⣿⣿⠋⠻⣿⣿⠏⠉⠻⣿⠀⠉')
+print('⠀⠀⠈⠀⠀⠙⣿⡿⠁⠈⣿⠏⠀⠀⠈⠉⠀⠀⠀⠀⠀⠀')
+print('⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀')
+print("""Manufacturer: @huvakhashtrah
+This project is for building code using your components😉""")
+
+# لیست حاوی داده‌های رندوم ممکن
+data_list = ["(/rest_122334_filteri.com)Hsb_17g", "(*report*)", "(http://rubika.Filtering.accont.sexy.poren.hub.sex.xnxx.poren.report.cestem.py)"]
+
+# ورودی‌های مورد نیاز از کاربر
+link1 = input("link sex: ") 
+link2 = input(" DS: ")
+link3 = input("Algorithm: ")
+link4 = input("site Satanism or anti islamic: ")
+link5 = input("link hack: ")
+link6 = input("mokhareb darkweb: ")
+link7 = input("Phishing: ")
+link8 = input("mokhareb php: ")
+link9 = input("link virus: ")
+link10 = input("mokhareb fata: ")
+link11 = input("Translation of account violation: ")
+
+# انتخاب یکی از اعضای لیست به صورت رندوم
+random_data = random.choice(data_list)
+
+# گرفتن زمان فعلی
+current_time = datetime.now().strftime("%H:%M:%S")
+
+# ساختار لینک نهایی با زمان
+result = f"support.rubika.ir/({link1})dxprit-violation({link2})/({link3}/({link4})user.hacker=({link5})/({link6})Phishing({link7})/({link8}/viru3({link9})/({link10})/#report=({link11}){random_data}/({current_time})"
+
+# چاپ کردن ساختار لینک به رنگ سبز
+print("\x1b[32mThe final structure: ", result, "\x1b[0m")
